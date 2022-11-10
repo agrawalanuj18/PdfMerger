c29c8748891c926de6f5429cae7cd63a61c52c4d
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-11-10 22:32:21 +0530

    add

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"));
 })
 
+app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
+  console.log(req.files)
+  res.send({data: req.files})
+
+  // req.files is array of `pdfs` files
+  // req.body will contain the text fields, if there were any
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port https://localhost:${port}`)
 })
